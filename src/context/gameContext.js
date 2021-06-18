28711f2022d3a0d85a57c2ae34e84a75db57d7b1
Malinali Becerril <mali@devf.mx>
2021-06-17 22:40:31 -0500

    add petitions

--- src/context/gameContext.js
+++ src/context/gameContext.js
@@ -74,7 +74,6 @@ const GameProvider = ({ children }) => {
     axios({
       method: "post",
       url: 'http://localhost:8080/CrudFracciones/DeleteLevel?id='+id,
-      data: {},
       headers: {"Content-Type": "multipart/form-data"}
     })
       .then(({data, status})=> {
